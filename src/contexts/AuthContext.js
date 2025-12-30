3cc2237fc6cbc252210f86038d437d869bfb562e
mibra7697-svg <mibra7697@gmail.com>
2025-12-31 02:11:20 +0300

    final fix for white screen

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useContext } from 'react';
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  return (
+    <AuthContext.Provider value={{ user: null, loading: false }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
